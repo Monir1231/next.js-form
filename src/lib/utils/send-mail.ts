0dd74a0c05662edf82461ca6b10f89e76d26ac60
Monir1231 <monirmolla324@gmail.com>
2025-10-26 08:21:45 +0600

    Add nodemailer dependency and implement sendmail utility function
    - Updated package.json to include nodemailer and its types.
    - Created send-mail.ts utility to configure nodemailer for sending emails using Ethereal SMTP service.

---
+++ src/lib/utils/send-mail.ts
@@ -0,0 +1,20 @@
+import nodemailer from 'nodemailer';
+
+export const sendmail = async () =>{
+    const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "maddison53@ethereal.email",
+    pass: "jn7jnAPss4f63QBp6D",
+  },
+});
+const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch" <maddison53@ethereal.email>',
+    to: "bar@example.com, baz@example.com",
+    subject: "Hello ✔",
+    html: "<b>Hello world?</b>", // HTML body
+  });
+
+}
